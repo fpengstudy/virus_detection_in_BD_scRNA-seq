57f9cb60742a79b5674d3432a9091ffe80c0bcc1
fpengstudy <88874390+fpengstudy@users.noreply.github.com>
2024-01-12 15:40:51 +0800

    Create only_bk.py

---
+++ only_bk.py
@@ -0,0 +1,26 @@
+import sys
+
+def only_bk(path):
+    with open(path,"r") as f:
+        f1 = f.readlines()
+    result = ""
+    num = 1
+    last_content = f1[0]
+    for i in f1[1:]:
+
+        if i.split("\t")[0] == last_content.split("\t")[0] and abs(int(i.split("\t")[1])-int(last_content.split("\t")[1]))<=20:
+            num +=1
+        elif i==f1[-1] and i.split("\t")[0] == last_content.split("\t")[0] and abs(int(i.split("\t")[1])-int(last_content.split("\t")[1]))<=20:
+            result += last_content.split("\t")[0]+"\t" + last_content.split("\t")[1]+"\t"+ str(num+1)+"\n"
+        elif i==f1[-1] and i.split("\t")[0] != last_content.split("\t")[0]:
+            result += last_content.split("\t")[0]+"\t" + last_content.split("\t")[1]+"\t"+ str(num)+"\n"+i.split("\t")[0] + "\t" + i.split("\t")[1] + "\t" + str(num) + "\n"
+        else:
+            result += last_content.split("\t")[0]+"\t" + last_content.split("\t")[1]+"\t"+ str(num)+"\n"
+            num =1
+            last_content = i
+    print(result)
+
+
+if __name__ == "__main__":
+    argv = sys.argv[1]
+    last_result = only_bk(argv)
